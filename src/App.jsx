98e93f8d2a4957f1c57072504bf37326de5f8810
jacquelyn-bradshaw <jacquelyn_bradshaw@live.co.uk>
2024-04-17 14:32:15 +0100

    using derived state to manage turns

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,26 @@ import { useState } from "react";
 
 import Player from "./Components/Player";
 import GameBoard from "./Components/GameBoard";
+import Log from "./Components/Log";
 
 function App() {
+  const [gameTurns, setGameTurns] = useState([]);
   const [activePlayer, setActivePlayer] = useState("X");
 
-  function handleSelectSquare() {
+  function handleSelectSquare(rowIndex, colIndex) {
     setActivePlayer(curActivePlayer => curActivePlayer === "X" ? "O" : "X");
+    setGameTurns(prevTurns => {
+      let currentPlayer = "X";
+
+      if (prevTurns.length > 0 && prevTurns[0].player === "X") {
+        currentPlayer = "O";
+      }
+      const updatedTurns = [
+        { square: { row: rowIndex, col: colIndex }, player: currentPlayer}, ...prevTurns
+      ];
+
+      return updatedTurns;
+    });
   }
 
   return (
@@ -17,11 +31,11 @@ function App() {
           <Player initialName="Player 1" symbol="X" isActive={activePlayer === "X"} />
           <Player initialName="Player 2" symbol="O" isActive={activePlayer === "O"} />
         </ol>
-        <GameBoard onSelectSquare={handleSelectSquare} activePlayerSymbol={activePlayer} />
+        <GameBoard onSelectSquare={handleSelectSquare} turns={gameTurns} />
       </div>
-      LOG
+      <Log turns={gameTurns}/>
     </main>
   )
 }
 
-export default App
+export default App;
